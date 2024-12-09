68e43fd7edf90d3f29b5d3a488b28ad07e0ef1f1
fauzihub13 <fauziadisaputra3@gmail.com>
2024-12-09 22:47:15 +0700

    feat: ui pop up on succes calculate

--- lib/src/presentation/suncost/dialogs/suncost_result_dialog.dart
+++ lib/src/presentation/suncost/dialogs/suncost_result_dialog.dart
@@ -27,107 +27,21 @@ class _SunCostResultDialogState extends State<SunCostResultDialog> {
               ),
               const Center(
                 child: Text(
-                  'Berhasil membuat jadwal',
+                  'Berhasil menghitung estimasi',
                   textAlign: TextAlign.center,
                   style: TextStyle(fontSize: 16.0),
                 ),
               ),
               const SizedBox(
-                height: 24.0,
+                height: 30.0,
               ),
-              const Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'KODE BOOKING',
-                    style: TextStyle(color: AppColors.grey),
-                  )),
-              const SizedBox(
-                height: 4,
+              const Text(
+                'Total estimasi biaya Anda adalah Rp12.000.000',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16, color: AppColors.darkBlue),
               ),
-              // BlocBuilder<VendorBookingBloc, VendorBookingState>(
-              //   builder: (context, state) {
-              //     final bookingCode = state.maybeWhen(
-              //         orElse: () => '-',
-              //         success: (vendor) => vendor.codeBooking);
-              //     return Align(
-              //       alignment: Alignment.centerLeft,
-              //       child: Text(
-              //         bookingCode,
-              //         style: const TextStyle(fontWeight: FontWeight.w500),
-              //       ),
-              //     );
-              //   },
-              // ),
               const SizedBox(
-                height: 4.0,
-              ),
-              const Divider(),
-              const SizedBox(
-                height: 8.0,
-              ),
-              const Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'VENDOR',
-                    style: TextStyle(color: AppColors.grey),
-                  )),
-              const SizedBox(
-                height: 4,
-              ),
-              // BlocBuilder<VendorBookingBloc, VendorBookingState>(
-              //   builder: (context, state) {
-              //     final vendorName = state.maybeWhen(
-              //         orElse: () => '-',
-              //         success: (vendor) => vendor.vendorName);
-              //     return Align(
-              //       alignment: Alignment.centerLeft,
-              //       child: Text(
-              //         vendorName,
-              //         style: const TextStyle(fontWeight: FontWeight.w500),
-              //       ),
-              //     );
-              //   },
-              // ),
-              const SizedBox(
-                height: 4.0,
-              ),
-              const Divider(),
-              const SizedBox(
-                height: 8.0,
-              ),
-              const Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'TANGGAL BOOKING',
-                    style: TextStyle(color: AppColors.grey),
-                  )),
-              const SizedBox(
-                height: 4,
-              ),
-              // BlocBuilder<VendorBookingBloc, VendorBookingState>(
-              //   builder: (context, state) {
-              //     final bookingDate = state.maybeWhen(
-              //       orElse: () => '-',
-              //       success: (vendor) {
-              //         return DateFormat('dd MMMM yyyy')
-              //             .format(vendor.bookingDate);
-              //       },
-              //     );
-              //     return Align(
-              //       alignment: Alignment.centerLeft,
-              //       child: Text(
-              //         bookingDate,
-              //         style: const TextStyle(fontWeight: FontWeight.w500),
-              //       ),
-              //     );
-              //   },
-              // ),
-              const SizedBox(
-                height: 4.0,
-              ),
-              const Divider(),
-              const SizedBox(
-                height: 20.0,
+                height: 36.0,
               ),
               Button.filled(
                   onPressed: () {
